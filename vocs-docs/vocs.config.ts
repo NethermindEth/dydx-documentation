2ef2475b4d2cb53430e77e137b41f0426f0e6b7f
Denis Kolodin <deniskolodin@gmail.com>
2025-04-14 13:54:47 +0200

    Fix the toc

--- vocs-docs/vocs.config.ts
+++ vocs-docs/vocs.config.ts
@@ -1,74 +1,344 @@
 import { defineConfig } from 'vocs'
 
 export default defineConfig({
-  title: 'dYdX Documentation',
-  basePath: '/dydx-documentation',
-  sidebar: [
-    {
-      text: 'Getting Started',
-      link: '/client',
-      items: [
+    title: 'dYdX Documentation',
+    basePath: '/dydx-documentation',
+    sidebar: [
         {
-          text: 'Quick start with Rust 🚧',
-          link: '/client/quick-start-rs',
+            text: 'Getting Started',
+            link: '/client',
+            items: [
+                {
+                    text: 'Quick start with Python 🚧',
+                    link: '/client/quick-start-py',
+                },
+                {
+                    text: 'Quick start with Rust 🚧',
+                    link: '/client/quick-start-rs',
+                },
+                {
+                    text: 'Quick start with TypeScript 🚧',
+                    link: '/client/quick-start-ts',
+                },
+                {
+                    text: 'Getting the mnemonic phrase',
+                    link: '/todo',
+                },
+            ],
         },
         {
-          text: 'Quick start with TypeScript 🕙',
-          link: '/client/quick-start-ts',
+            text: 'Interaction',
+            link: '/todo',
+            items: [
+                {
+                    text: 'Wallet Setup',
+                    link: '/todo',
+                },
+                {
+                    text: 'Available clients',
+                    link: '/todo',
+                },
+                {
+                    text: 'Trading',
+                    link: '/todo',
+                    items: [
+                        {
+                            text: 'Placing an order',
+                            link: '/todo',
+                        },
+                        {
+                            text: 'Cancelling an order',
+                            link: '/todo',
+                        },
+                    ],
+                },
+                {
+                    text: 'Asset management',
+                    link: '/todo',
+                },
+                {
+                    text: 'Data',
+                    link: '/todo',
+                    items: [
+                        {
+                            text: 'Account balance',
+                            link: '/todo',
+                        },
+                        {
+                            text: 'List orders',
+                            link: '/todo',
+                        },
+                        {
+                            text: 'List positions',
+                            link: '/todo',
+                        },
+                        {
+                            text: 'List fills',
+                            link: '/todo',
+                        },
+                        {
+                            text: 'List markets',
+                            link: '/todo',
+                        },
+                        {
+                            text: 'Block height',
+                            link: '/todo',
+                        },
+                        {
+                            text: 'Trading rewards',
+                            link: '/todo',
+                        },
+                        {
+                            text: 'Fee tiers',
+                            link: '/todo',
+                        },
+                        {
+                            text: 'Reward parameters',
+                            link: '/todo',
+                        },
+                        {
+                            text: 'Watch an Orderbook',
+                            link: '/todo',
+                        },
+                    ],
+                },
+                {
+                    text: 'MegaVault',
+                    link: '/todo',
+                    items: [
+                        {
+                            text: 'Deposit',
+                            link: '/todo',
+                        },
+                        {
+                            text: 'Withdraw',
+                            link: '/todo',
+                        },
+                    ],
+                },
+                {
+                    text: 'Permissioned Keys',
+                    link: '/todo',
+                    items: [
+                        {
+                            text: 'Creating an authenticator',
+                            link: '/todo',
+                        },
+                        {
+                            text: 'Add authenticator',
+                            link: '/todo',
+                        },
+                        {
+                            text: 'Remove authenticator',
+                            link: '/todo',
+                        },
+                        {
+                            text: 'Permissioned trading',
+                            link: '/todo',
+                        },
+                    ],
+                },
+                {
+                    text: 'Noble client',
+                    link: '/todo',
+                },
+                {
+                    text: 'Faucet client',
+                    link: '/todo',
+                },
+                {
+                    text: '',
+                    link: '/todo',
+                    items: [
+                        {
+                            text: '',
+                            link: '/todo',
+                        },
+                    ],
+                },
+
+
+
+
+
+                {
+                    text: '',
+                    link: '/todo',
+                },
+            ],
+        },
+        /*
+        {
+            text: 'Setup',
+            link: '/setup',
+            items: [
+                {
+                    text: 'Endpoints',
+                    link: '/endpoints',
+                },
+                {
+                    text: 'Wallet Setup',
+                    link: '/wallet-setup',
+                },
+                {
+                    text: 'Clients',
+                    link: '/clients',
+                },
+            ]
         },
         {
-          text: 'Quick start with Python 🚧',
-          link: '/client/quick-start-py',
+            text: 'API Map',
+            link: '/api-map',
         },
-      ],
-    },
-    {
-      text: 'Setup',
-      link: '/setup',
-      items: [
-          {
-            text: 'Endpoints',
-            link: '/endpoints',
-          },
-          {
-            text: 'Wallet Setup',
-            link: '/wallet-setup',
-          },
-          {
-            text: 'Clients',
-            link: '/clients',
-          },
-      ]
-    },
-    {
-        text: 'API Map',
-        link: '/api-map',
-    },
-    {
-        text: 'Node Client',
-        link: '/node-client',
-    },
-    {
-        text: 'Indexer Client',
-        link: '/indexer-client',
-    },
-    {
-      text: 'Trading',
-      link: '/api',
-      items: [
-          {
-              text: 'Order Parameters 🚧',
-              link: '/api/order_parameters',
-          },
-          {
-              text: 'Placing An Order 🚧',
-              link: '/api/place_order',
-          },
-          {
-              text: 'Cancelling An Order 🚧',
-              link: '/api/cancel_order',
-          },
-      ]
-    },
-  ],
+        */
+        {
+            text: 'Node Client',
+            link: '/node-client/intro',
+            items: [
+            ],
+        },
+        {
+            text: 'Full API',
+            link: '/api',
+            items: [
+                {
+                    text: 'Node API',
+                    link: '/node-client',
+                },
+                {
+                    text: 'Indexer API',
+                    link: '/indexer-client',
+                },
+            ],
+        },
+        /*
+        {
+            text: 'Trading',
+            link: '/api',
+            items: [
+                {
+                    text: 'Order Parameters 🚧',
+                    link: '/api/order_parameters',
+                },
+                {
+                    text: 'Placing An Order 🚧',
+                    link: '/api/place_order',
+                },
+                {
+                    text: 'Cancelling An Order 🚧',
+                    link: '/api/cancel_order',
+                },
+            ]
+        },
+        */
+    ],
 })
+
+/*
+Clients +
+    JavaScript / TypeScript +
+    Python +
+    Rust +
+    Getting the mnemonic phrase
+
+API: functional
+    Wallet setup
+    Available clients
+    Trading
+        Placing an order
+        Cancelling an order
+    Asset management
+    Data
+        Account balance
+        List orders
+        List positions
+        List fills
+        List markets
+        Block height
+        Trading rewards
+        Fee tiers
+        Reward parameters
+        Create orderbook?
+    MegaVault
+        Deposit
+        Withdraw
+    Permissioned Keys
+        Creating an authenticator
+        Add authenticator
+        Remove authenticator
+        Permissioned trading
+    Noble client
+    Faucet client
+
+Architecture
+    Structure
+        Validators (aka Nodes)
+        Indexer
+        Networks
+    Trading / Perpetual Contracts
+        Accounts and Subaccounts
+        Assets and Perpetuals
+        Orders
+        Margin
+        Funding
+        Price data
+        Permissioned keys / Authenticators
+        MegaVault
+        Markets
+API: doc all methods
+    HTTP API
+    WebSockets
+    gRPC
+Policies
+    Security
+    Terms of Service and Privacy
+*/
+
+
+/*
+Onboarding
+Configuring a Network
+Initialize Client
+Setup Mnemonic
+Transfer
+Deposit
+Withdraw
+Simulate a Transaction
+Sign a Transaction
+Send a Transaction
+Selecting desired gas token
+Get Account Balances
+Placing an Order
+Replacing an Order
+Cancelling an Order
+*/
+
+/*
+GetAddress
+GetSubaccount
+GetParentSubaccount
+GetAssetPositions
+GetAssetPositionsForParentSubaccount
+GetCandles
+Screen
+GetFills
+GetFillsForParentSubaccount
+GetHeight
+GetTradingRewards
+GetHistoricalFunding
+GetHistoricalPnl
+GetHistoricalPnlForParentSubaccount
+GetAggregations
+GetPerpetualMarket
+ListOrders
+ListOrdersForParentSubaccount
+GetOrder
+ListPerpetualMarkets
+ListPositions
+ListPositionsForParentSubaccount
+Get
+GetTime
+GetTrades
+GetTransfers
+GetTransfersForParentSubacc
+*/
