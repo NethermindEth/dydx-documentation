90184e8a6ed4785c41854f9f195d18b56b01162f
v0-e <xyz.vieira@gmail.com>
2025-06-09 14:07:00 +0100

    Re-add Indexer API page

--- vocs-docs/vocs.config.ts
+++ vocs-docs/vocs.config.ts
@@ -77,6 +77,7 @@ export default defineConfig({
             items: [
                 {
                     text: 'Indexer API',
+                    link: '/indexer-client',
                     collapsed: true,
                     items: [
                         {
